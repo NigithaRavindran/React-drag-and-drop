666da35ca4f3a4fd7e8065bff27527facd49ee75
Nigitha <nigithar@gmail.com>
2020-12-01 22:27:00 +0530

    Committed changes

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import DragContainer from "./components/DragContainer";
+
+import "./sass/app.scss";
+
+const App = () => {
+  const [savedValue, getSavedValue] = useState()
+
+  const onSubmit = () => {
+    getDataFromLocalStorage()
+  };
+
+  const getDataFromLocalStorage = () => {
+    let el = document.querySelector(".drop-container");
+    let elHTML = el.innerHTML;
+    localStorage.setItem("itemsaved", elHTML);
+  }
+
+  useEffect(() => {
+    let getLocalStorage = localStorage.getItem("itemsaved")
+    getSavedValue(getLocalStorage)
+    let el = document.querySelector(".drop-container");
+    el.innerHTML = getLocalStorage
+  }, [savedValue])
+
+  return (
+    <main className="builder">
+      <section>
+        <form>
+          <DragContainer />
+        </form>
+      </section>
+      <section>
+        <button onClick={onSubmit} type="submit" className="submit">
+          Save
+        </button>
+      </section>
+    </main>
+  );
+};
+
+export default App;
